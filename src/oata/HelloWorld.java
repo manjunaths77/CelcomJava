7d791ddbb4845b7cd3f57c3260f3fbbb5e58f105
manjunaths77 <85163566+manjunaths77@users.noreply.github.com>
2021-06-21 19:42:09 +0530

    Update HelloWorld.java

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,6 +2,6 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World welcome to the Dev branch. Welcome here");
+        System.out.println("Hello World welcome to the Dev branch. Welcome here 1");
     }
 }
